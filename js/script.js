681aa295a3e1a398954a0ebd642c63775df9acbe
Manuel Bonini <manuelbonini17@gmail.com>
2021-04-19 17:24:52 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            message: 'Ciao, sono un messaggio dentro h1'
+            message: 'Ciao, sono un messaggio dentro h1',
+            myImage: 'img/landscape.jpg'
         }
     }
 );
